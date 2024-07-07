7bcc07ff172ba719dba2175037d721a9114097c5
NieTsuki <nie.0x6708@gmail.com>
2024-07-07 14:09:35 +0800

    Add cute background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
+<style>
+    #bg {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+        background-size: cover;
+        background-image: url(japanese-streets-with-modern-houses/4248613.jpg);
+    }
+</style>
+
+<div id="bg"></div>
+
 <img
     src="https://media.tenor.com/G_n-KffB79YAAAAi/shake-anime.gif"
     alt=""
-    class="absolute bottom-0 left-[50px]"
+    width=300
+    class="absolute bottom-0 left-[150px]"
 />
